c2a9810a64c5e2096d5cf8fbc17a11e42e69a1dc
gmapwata6@gmail.com <gmapwata6@gmail.com>
2023-09-19 17:50:33 +0100

    feat:add new feature archiver

--- utils/backup.js
+++ utils/backup.js
@@ -1,24 +1,8 @@
-const knex = require('knex');
 const knexConfig = require('../knexfile');
-const db = knex(knexConfig.development);
 const { exec } = require('child_process');
 const generateName = require ('./generateNameFile');
 const {FILE_PATH , TO_ARCHIVE} = require ('../utils/utilities')
 
-
-// const filePath = process.env.FILE_PATH
-
-
-// // Commande de sauvegarde avec mysqldump
-// const commandeSauvegarde = `mysqldump --host=${knexConfig.development.connection.host} --user=${knexConfig.development.connection.user} --password=${knexConfig.development.connection.password} ${knexConfig.development.connection.database} > ${generateName.generateNameFile(FILE_PATH, TO_ARCHIVE)}`;
-
-// exec(commandeSauvegarde, (erreur, stdout, stderr) => {
-//   if (erreur) {
-//     console.error(`Erreur lors de la sauvegarde : ${erreur}`);
-//   } else {
-//     console.log('Sauvegarde réussie.');
-//   }
-// });
 
 const backupCommand = async () => {
   try {
